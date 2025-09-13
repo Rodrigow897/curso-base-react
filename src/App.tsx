108eef3b46db5102501eddeb7d26eac6fae4f4f7
Rodrigo <rwesley2000@gmail.com>
2025-09-13 15:54:04 -0300

    learning about list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 
 
 export function App() {
+  const [value, setValue] = useState('');
+  const [list, setList] = useState([
+    { id: '1', label: 'Fazer cafe' },
+    { id: '2', label: 'Fazer cafe' },
+    { id: '3', label: 'Fazer almoço' },
+    { id: '4', label: 'Fazer janta' },
+  ]);
+
   return (
-    <>
-      Olá
-    </>
+    <div>
+      
+      <input value={value} onChange={(e)=>setValue(e.target.value)} type="text" />
+      <button onClick={() => {setList([
+        ...list, { id: String(list.length + 1), label: value }
+      ]);
+      setValue('');
+      }} >Adicionar</button>
+
+      <ol>
+        {list.map((listitem) => (
+          <li key={listitem.id}>{listitem.label}</li>
+        ))}
+      </ol>
+    </div>  
   )
 }
