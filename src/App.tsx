eeacaac3f0cfb9b8c062e5f350f24067524ab2b4
Rodrigo <rwesley2000@gmail.com>
2025-09-14 23:44:53 -0300

    feat: added remover and concluido button

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import { useState } from "react";
 export function App() {
   const [value, setValue] = useState('');
   const [list, setList] = useState([
-    { id: '1', label: 'Fazer cafe' },
-    { id: '2', label: 'Fazer cafe' },
-    { id: '3', label: 'Fazer almoço' },
-    { id: '4', label: 'Fazer janta' },
+    { id: '1', label: 'Fazer cafe', complete: false },
+    { id: '2', label: 'Fazer cafe', complete: false },
+    { id: '3', label: 'Fazer almoço', complete: false },
+    { id: '4', label: 'Fazer janta', complete: false },
   ]);
 
   return (
@@ -15,14 +15,28 @@ export function App() {
       
       <input value={value} onChange={(e)=>setValue(e.target.value)} type="text" />
       <button onClick={() => {setList([
-        ...list, { id: String(list.length + 1), label: value }
+        ...list, { id: String(list.length + 1), label: value, complete: false}
       ]);
       setValue('');
       }} >Adicionar</button>
 
       <ol>
         {list.map((listitem) => (
-          <li key={listitem.id}>{listitem.label}</li>
+          <li key={listitem.id}>
+            {listitem.label}
+
+            {listitem.complete ? <span>(concluido)</span> : ''}
+          
+          <button onClick={()=>{
+              setList(list.map(item =>({...item, complete: item.id === listitem.id ? true : item.complete})))}}>
+              Concliur
+            </button>
+
+            <button onClick={()=>{
+              setList(list.filter(item => item.id !== listitem.id))}}>
+              Remover
+            </button>
+          </li>
         ))}
       </ol>
     </div>  
